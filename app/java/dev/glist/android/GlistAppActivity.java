2b00cc718997471e5d9642700e812fd4d3967f60
Metehan Gezer <irrelevantdev@outlook.com>
2023-07-27 14:50:57 +0300

    Clean imports

--- app/java/dev/glist/android/GlistAppActivity.java
+++ app/java/dev/glist/android/GlistAppActivity.java
@@ -1,22 +1,6 @@
 package dev.glist.android;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-import org.fmod.FMOD;
-
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
-import dev.glist.android.lib.GlistNative;
 
 public class GlistAppActivity extends BaseGlistAppActivity {
 
